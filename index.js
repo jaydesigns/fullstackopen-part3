3bc7e6687a9cab5bb535f99bda3fa985741573fa
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2022-08-24 22:44:47 -0600

    Added front-end production build

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('build'))
 morgan.token('dat', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dat'))
 
