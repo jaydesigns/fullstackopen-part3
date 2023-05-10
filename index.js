27ae051427eb6cb31dbae393f498356a282fe5fc
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2023-05-09 23:07:36 -0600

    uibuild

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ morgan.token('reqBody',function(req,res){return JSON.stringify(req.body)})
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 
+app.use(express.static('build'))
+
 let persons = [
     { 
       "id": 1,
